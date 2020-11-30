4af961fbc775c8d31bd8114d414ff39a7a7e5b81
xin <1713695034@qq.com>
2020-11-30 11:22:31 +0800

    java是世界上最好的语言！

---
+++ java666/src/java666/JavaAndLinuxAndC666.java
@@ -0,0 +1,11 @@
+package java666;
+
+public class JavaAndLinuxAndC666 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("java是世界上最好的语言！");
+System.out.println("C语言："+"\n"+"大佬喝茶，向大佬膜拜！");
+	}
+
+}
